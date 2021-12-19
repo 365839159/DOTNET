4076858640d410733b58c14912f4d09abfe66b5e
365839159 <365839159@qq.com>
2021-12-19 21:59:34 +0800

    zxc

--- DOTNET/NET/NoSql/MongoDB/MongoDBSample/Controllers/mongoDriverSampleController.cs
+++ DOTNET/NET/NoSql/MongoDB/MongoDBSample/Controllers/mongoDriverSampleController.cs
@@ -1,19 +1,67 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
 using MongoDB.Driver;
-
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 namespace MongoDBSample.Controllers
 {
     public class mongoDriverSampleController : BaseController
     {
         private readonly ILogger<MongoDbSampleController> _logger;
-        private readonly IMongoClient _mongoClient = new MongoClient("mongodb://47.94.85.108:27017");
+        private readonly IMongoClient _mongoClient = new MongoClient("mongodb://localhost:27017");
         private readonly IMongoDatabase _mongoDatabase;
 
         public mongoDriverSampleController(ILogger<MongoDbSampleController> logger)
         {
             _logger = logger;
-            _mongoDatabase = _mongoClient.GetDatabase("mongodbSample");
+            _mongoDatabase = _mongoClient.GetDatabase("zxc");
         }
+
+        /// <summary>
+        /// 查询所有文档
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<object> FindAll(Dictionary<string, object> pram)
+        {
+
+            //var zoneListStr = JsonConvert.SerializeObject(pram);
+            //var zoneList = JsonConvert.DeserializeObject<B>(zoneListStr);
+
+            foreach (var item in pram)
+            {
+
+            }
+
+            List<Dictionary<string, object>> filterList = new List<Dictionary<string, object>>();
+            var filter = Builders<BsonDocument>.Filter.Empty;
+            var projection = Builders<BsonDocument>.Projection.Exclude("_id");
+            var collection = _mongoDatabase.GetCollection<BsonDocument>("inventory");
+            var result = collection.Find(filter).Project(projection).ToList();
+            List<Dictionary<string, object>> data = new List<Dictionary<string, object>>();
+            foreach (var item in result)
+            {
+                var dic = item.ToDictionary();
+                data.Add(dic);
+            }
+
+            return data;
+        }
+
+        public void Digui(List<string> filterList, Dictionary<string, object> keys,Dictionary<string,object> obj)
+        {
+            if (keys["Chinds"] != null)
+                Digui(filterList, JsonConvert.DeserializeObject<Dictionary<string, object>>(keys["Chinds"].ToString()), obj);
+        }
+
+    }
+
+    public class B
+    {
+        public int Id { get; set; }
+        public string Zone { get; set; }
+        public object Chinds { get; set; }
     }
 }
---
+++ NetCore/NetCoreStudy/KnowledgePoints/IOC/yzk/IOCDemo.zyk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IOCDemo.zyk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
