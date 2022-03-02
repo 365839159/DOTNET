a0f3143f4d380b4a2f290a105186ae4d7c52e539
365839159 <365839159@qq.com>
2022-03-02 18:39:52 +0800

    rabbit

--- NoSqls/NoSqls/helloworld.Send/Program.cs
+++ NoSqls/NoSqls/helloworld.Send/Program.cs
@@ -6,19 +6,23 @@ Console.WriteLine("请输入要发送的内容！");
 string content;
 while ((content = Console.ReadLine()) != "q")
 {
+    //连接工厂
     var factory = new ConnectionFactory() { HostName = "localhost" };
+    //通过连接工厂创建连接
     using (var connection = factory.CreateConnection())
+    //创建信道
     using (var channel = connection.CreateModel())
     {
+        //定义队列
         channel.QueueDeclare(queue: "hello",
                              durable: false,
                              exclusive: false,
                              autoDelete: false,
                              arguments: null);
-
+        //将消息转换为二进制数据
         string message = content;
         var body = Encoding.UTF8.GetBytes(message);
-
+        //发布
         channel.BasicPublish(exchange: "",
                              routingKey: "hello",
                              basicProperties: null,
