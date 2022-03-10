eda2e295c9470d6642fcbd42335a0784e47e480b
365839159 <365839159@qq.com>
2022-03-10 22:16:08 +0800

    rabbitmq

---
+++ NoSqls/NoSqls/SubscribeOne/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    Uri = new Uri("amqp://admin:admin@192.168.65.133:5672"),
+    AutomaticRecoveryEnabled = true
+};
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    //创建交换机
+    var exchange = "HuaWeiP30";
+    channel.ExchangeDeclare(exchange: exchange, type: ExchangeType.Fanout);
+    //生成队列
+    var queueName = channel.QueueDeclare("SubscribeOne").QueueName;
+    channel.QueueBind(queue: queueName,
+                      exchange: exchange,
+                      routingKey: "");
+
+    Console.WriteLine($"{queueName}: Waiting for HuaWeiP30.");
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($"{queueName}: {message}");
+        Console.WriteLine($"{queueName}: 我要买一台");
+    };
+    channel.BasicConsume(queue: queueName,
+                         autoAck: true,
+                         consumer: consumer);
+
+    Console.WriteLine("按任意键退出！");
+    Console.ReadLine();
+}
