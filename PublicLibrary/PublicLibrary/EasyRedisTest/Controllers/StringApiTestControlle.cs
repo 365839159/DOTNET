569b33dbd361207edc44cb884bea1adf39aac23c
365839159 <365839159@qq.com>
2022-04-13 21:20:52 +0800

    zxc

---
+++ PublicLibrary/PublicLibrary/EasyRedisTest/Controllers/StringApiTestControlle.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleEasyRedis;
+
+namespace EasyRedisTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class StringApiTestControlle : ControllerBase
+    {
+        private readonly IEasyRedisProvider easyRedisProvider;
+        public StringApiTestControlle(IEasyRedisProvider easyRedisProvider)
+        {
+            this.easyRedisProvider = easyRedisProvider;
+        }
+        [HttpPost]
+        public async Task<bool> Set(string key, string value)
+        {
+            return await easyRedisProvider.Set(key, value);
+        }
+        [HttpPost]
+        public async Task<bool> Del(string key)
+        {
+            return await easyRedisProvider.Del(new List<string>() { key });
+        }
+    }
+}
